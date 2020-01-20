7838e33456053c0bde6fa391d1e7eae097f78318
takafk9 <narikawa.t@wano.co.jp>
2020-01-21 00:50:18 +0900

    add 0141's another answer

--- Algorithms/0141.linked_list_cycle/linked_list_cycle.go
+++ Algorithms/0141.linked_list_cycle/linked_list_cycle.go
@@ -3,30 +3,62 @@ package _141_linked_list_cycle
 /*
  * Definition for singly-linked list.
 */
-//type ListNode struct {
-//     Val int
-//     Next *ListNode
+
+
+/*
+Approach 1: Hash Table
+
+Time complexity : O(n)
+Space complexity: O(n)
+*/
+type ListNode struct {
+   Val int
+   Next *ListNode
+}
+//
+//type NodesSeen []*ListNode
+//
+//func hasCycle(head *ListNode) bool {
+//	nodesSeen := NodesSeen{}
+//	for head != nil{
+//		if nodesSeen.contain(head) {
+//			return true
+//		}
+//		nodesSeen = append(nodesSeen,head)
+//		head = head.Next
+//	}
+//	return false
+//}
+//
+//func(n NodesSeen) contain(head *ListNode) bool{
+//	for _,val := range n{
+//		if *val == *head{
+//			return true
+//		}
+//	}
+//	return false
 //}
 
-type NodesSeen []*ListNode
+/*
+Approach 2: Two Pointers
 
+time complexity is O(N+K), which is O(n)
+Space complexity : O(1).
+*/
 func hasCycle(head *ListNode) bool {
-	nodesSeen := NodesSeen{}
-	for head != nil{
-		if nodesSeen.contain(head) {
-			return true
-		}
-		nodesSeen = append(nodesSeen,head)
-		head = head.Next
+	if head == nil {
+		return false
 	}
-	return false
-}
-
-func(n NodesSeen) contain(head *ListNode) bool{
-	for _,val := range n{
-		if *val == *head{
+	slow := head
+	fast := head.Next
+	//Check whether the fast runner will eventually meet the slow runner or not
+	//if not, head has no cycle
+	for slow != nil && fast != nil && fast.Next != nil{
+		if *slow == *fast {
 			return true
 		}
+		slow = slow.Next
+		fast = fast.Next.Next
 	}
 	return false
 }
